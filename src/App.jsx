0463ced0e8bcecbfc0057c92f20cd9cf15e55c6a
Skylar-Lorena <lorayne911@gmail.com>
2026-04-28 10:31:12 +0300

    feat: add click event handler

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import './App.css'
 import Greeting from './Greeting';
 
 function App() {
+const handleClick = ()=>{
+  alert("Clicked!")
+}
+
   return (
     //jsx allows js inside of UI
     //components can receive data- we call that props
     <>
     <h1 className='hero'>Welcome to the Student Board</h1>
   <Greeting name ="Eugene"/> 
+  <button onClick={handleClick}>
+      Click Me!
+    </button>
 </>
   )
 }
